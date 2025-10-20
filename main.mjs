f1f2a7377014028ecbda7aaf31bc6bd0724cb356
denis dupont <denis.dupont@univ-evry.fr>
2025-10-20 13:15:28 +0200

    first commit

---
+++ main.mjs
@@ -0,0 +1,14 @@
+const handleSelect = function (event) {
+  const targetElement = event.target; // span ou li ou ul
+
+  if (targetElement.closest("div")) {
+    const selectedDiv = targetElement.closest(".container > div");
+    // Toggle selected class
+    selectedDiv.classList.toggle("selected");
+    const selectedValue = selectedDiv.dataset.value;
+    console.log(`Vous avez sélectionné : ${selectedValue}`);
+  }
+};
+const container = document.querySelector(".container");
+
+container.addEventListener("click", handleSelect, false);
